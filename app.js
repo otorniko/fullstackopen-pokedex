83d6b96b8d048e02ff80eae9b88959afb6b63d3d
otorniko <120563288+otorniko@users.noreply.github.com>
2023-05-30 13:34:12 +0300

    cypress vol2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
-//const process = require('process')
+const process = require('process')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = /* process.env.PORT  || */ 5000
+const PORT =  process.env.PORT  ||  5000
 
 app.use(express.static('dist'))
 
